827f6709cf2b5de80ce2f700bd659940d331b42c
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-01-08 11:48:32 +0100

    cleanup

--- src/main/java/com/creativemd/cmdcam/client/CamEventHandlerClient.java
+++ src/main/java/com/creativemd/cmdcam/client/CamEventHandlerClient.java
@@ -19,6 +19,7 @@ import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.client.settings.GameSettings;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.client.event.EntityViewRenderEvent.CameraSetup;
@@ -31,64 +32,64 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
 
 public class CamEventHandlerClient {
-    
+
     public static Minecraft mc = Minecraft.getMinecraft();
     public static float defaultfov = 70.0F;
     public static float lastFOV = 0;
     public static final float amountZoom = 0.1F;
     public static final float amountroll = 0.5F;
-    
+
     public static float fov;
     public static float roll = 0;
-    
+
     @SubscribeEvent
     public void onRenderTick(RenderTickEvent event) {
         if (mc.world == null)
             CMDCamClient.isInstalledOnSever = false;
-        
+
         if (lastFOV != 0 && lastFOV != mc.gameSettings.fovSetting)
             defaultfov = mc.gameSettings.fovSetting;
-        
+
         if (mc.player != null && mc.world != null) {
             if (mc.inGameHasFocus) {
                 if (CMDCamClient.getCurrentPath() == null) {
-                    if (mc.gameSettings.isKeyDown(KeyHandler.zoomIn)) {
+                    if (GameSettings.isKeyDown(KeyHandler.zoomIn)) {
                         if (mc.player.isSneaking())
                             mc.gameSettings.fovSetting -= amountZoom * 10;
                         else
                             mc.gameSettings.fovSetting -= amountZoom;
                     }
-                    
-                    if (mc.gameSettings.isKeyDown(KeyHandler.zoomOut)) {
+
+                    if (GameSettings.isKeyDown(KeyHandler.zoomOut)) {
                         if (mc.player.isSneaking())
                             mc.gameSettings.fovSetting += amountZoom * 10;
                         else
                             mc.gameSettings.fovSetting += amountZoom;
                     }
-                    
-                    if (mc.gameSettings.isKeyDown(KeyHandler.zoomCenter)) {
+
+                    if (GameSettings.isKeyDown(KeyHandler.zoomCenter)) {
                         mc.gameSettings.fovSetting = defaultfov;
                     }
                     fov = mc.gameSettings.fovSetting;
-                    
-                    if (mc.gameSettings.isKeyDown(KeyHandler.rollLeft))
+
+                    if (GameSettings.isKeyDown(KeyHandler.rollLeft))
                         roll -= amountroll;
-                    
-                    if (mc.gameSettings.isKeyDown(KeyHandler.rollRight))
+
+                    if (GameSettings.isKeyDown(KeyHandler.rollRight))
                         roll += amountroll;
-                    
-                    if (mc.gameSettings.isKeyDown(KeyHandler.rollCenter))
+
+                    if (GameSettings.isKeyDown(KeyHandler.rollCenter))
                         roll = 0;
-                    
+
                     if (KeyHandler.pointKey.isPressed()) {
                         CMDCamClient.points.add(new CamPoint());
                         mc.player.sendMessage(new TextComponentString("Registered " + CMDCamClient.points.size() + ". Point!"));
                     }
-                    
+
                 } else {
                     CMDCamClient.tickPath(mc.world, event.renderTickTime);
                 }
-                
+
                 if (KeyHandler.startStop.isPressed()) {
                     if (CMDCamClient.getCurrentPath() != null) {
                         CMDCamClient.stopPath();
@@ -99,7 +100,7 @@ public class CamEventHandlerClient {
                             mc.player.sendMessage(new TextComponentString(e.getMessage()));
                         }
                 }
-                
+
                 while (KeyHandler.clearPoint.isPressed()) {
                     CMDCamClient.points.clear();
                     mc.player.sendMessage(new TextComponentString("Cleared all registered points!"));
@@ -109,7 +110,7 @@ public class CamEventHandlerClient {
         lastFOV = mc.gameSettings.fovSetting;
         lastRenderTime = System.nanoTime();
     }
-    
+
     @SubscribeEvent
     public void worldRender(RenderWorldLastEvent event) {
         boolean shouldRender = false;
@@ -124,7 +125,7 @@ public class CamEventHandlerClient {
             OpenGlHelper.glBlendFunc(770, 771, 1, 0);
             GlStateManager.disableTexture2D();
             GL11.glDepthMask(false);
-            
+
             Vec3[] points = new Vec3[CMDCamClient.points.size()];
             for (int i = 0; i < points.length; i++) {
                 points[i] = new Vec3(CMDCamClient.points.get(i).x, CMDCamClient.points.get(i).y, CMDCamClient.points.get(i).z);
@@ -134,28 +135,28 @@ public class CamEventHandlerClient {
                 renderBlock(points[i].x, points[i].y, points[i].z, 0.1, 0.1, 0.1, 0, 0, 0, 1, 1, 1, 1);
                 float f = TileEntityRendererDispatcher.instance.entityYaw;
                 float f1 = TileEntityRendererDispatcher.instance.entityPitch;
-                boolean flag = false;
+
                 EntityRenderer.drawNameplate(mc.fontRenderer, (i + 1) + "", (float) points[i].x, (float) points[i].y + 0.4F, (float) points[i].z, 0, f, f1, false, false);
                 GL11.glDepthMask(false);
                 GlStateManager.disableLighting();
                 GlStateManager.disableTexture2D();
                 GlStateManager.popMatrix();
             }
-            
+
             for (Iterator<CamInterpolation> iterator = CamInterpolation.interpolationTypes.values().iterator(); iterator.hasNext();) {
                 CamInterpolation movement = iterator.next();
                 if (movement.isRenderingEnabled)
                     renderMovement(movement, new ArrayList<>(CMDCamClient.points));
             }
-            
+
             GL11.glDepthMask(true);
             GlStateManager.enableTexture2D();
             GlStateManager.enableBlend();
             GlStateManager.resetColor();
-            
+
         }
     }
-    
+
     public static void renderBlock(double x, double y, double z, double width, double height, double length, double rotateX, double rotateY, double rotateZ, double red, double green, double blue, double alpha) {
         GlStateManager.pushMatrix();
         GlStateManager.translate(x, y, z);
@@ -165,7 +166,7 @@ public class CamEventHandlerClient {
         GlStateManager.rotate((float) rotateZ, 0, 0, 1);
         GlStateManager.scale(width, height, length);
         GlStateManager.color((float) red, (float) green, (float) blue, (float) alpha);
-        
+
         GlStateManager.glBegin(GL11.GL_POLYGON);
         GlStateManager.glNormal3f(0.0f, 1.0f, 0.0f);
         GlStateManager.glVertex3f(-0.5f, 0.5f, 0.5f);
@@ -174,7 +175,7 @@ public class CamEventHandlerClient {
         GlStateManager.glVertex3f(0.5f, 0.5f, -0.5f);
         GlStateManager.glVertex3f(-0.5f, 0.5f, -0.5f);
         GlStateManager.glEnd();
-        
+
         GL11.glBegin(GL11.GL_POLYGON);
         //GL11.glColor4d(red, green, blue, alpha);
         GlStateManager.glNormal3f(0.0f, 0.0f, 1.0f);
@@ -183,7 +184,7 @@ public class CamEventHandlerClient {
         GlStateManager.glVertex3f(-0.5f, 0.5f, 0.5f);
         GlStateManager.glVertex3f(-0.5f, -0.5f, 0.5f);
         GlStateManager.glEnd();
-        
+
         GL11.glBegin(GL11.GL_POLYGON);
         //GL11.glColor4d(red, green, blue, alpha);
         GlStateManager.glNormal3f(1.0f, 0.0f, 0.0f);
@@ -192,7 +193,7 @@ public class CamEventHandlerClient {
         GlStateManager.glVertex3f(0.5f, -0.5f, 0.5f);
         GlStateManager.glVertex3f(0.5f, -0.5f, -0.5f);
         GlStateManager.glEnd();
-        
+
         GL11.glBegin(GL11.GL_POLYGON);
         //GL11.glColor4d(red, green, blue, alpha);
         GlStateManager.glNormal3f(-1.0f, 0.0f, 0.0f);
@@ -201,7 +202,7 @@ public class CamEventHandlerClient {
         GlStateManager.glVertex3f(-0.5f, 0.5f, -0.5f);
         GlStateManager.glVertex3f(-0.5f, -0.5f, -0.5f);
         GlStateManager.glEnd();
-        
+
         GL11.glBegin(GL11.GL_POLYGON);
         //GL11.glColor4d(red, green, blue, alpha);
         GlStateManager.glNormal3f(0.0f, -1.0f, 0.0f);
@@ -210,7 +211,7 @@ public class CamEventHandlerClient {
         GlStateManager.glVertex3f(-0.5f, -0.5f, -0.5f);
         GlStateManager.glVertex3f(0.5f, -0.5f, -0.5f);
         GlStateManager.glEnd();
-        
+
         GL11.glBegin(GL11.GL_POLYGON);
         //GL11.glColor4d(red, green, blue, alpha);
         GlStateManager.glNormal3f(0.0f, 0.0f, -1.0f);
@@ -219,19 +220,19 @@ public class CamEventHandlerClient {
         GlStateManager.glVertex3f(-0.5f, -0.5f, -0.5f);
         GlStateManager.glVertex3f(-0.5f, 0.5f, -0.5f);
         GlStateManager.glEnd();
-        
+
         GlStateManager.popMatrix();
     }
-    
+
     public void renderMovement(CamInterpolation movement, ArrayList<CamPoint> points) {
         try {
             movement.initMovement(points, 0, CMDCamClient.target);
         } catch (PathParseException e) {
             return;
         }
-        
+
         double steps = 20 * (points.size() - 1);
-        
+
         GlStateManager.pushMatrix();
         Vec3 color = movement.getColor();
         GL11.glColor3d(color.x, color.y, color.z);
@@ -247,50 +248,50 @@ public class CamEventHandlerClient {
         GlStateManager.glEnd();
         GlStateManager.popMatrix();
     }
-    
+
     public Entity renderEntity;
-    
+
     @SubscribeEvent
     public void renderPlayerPre(RenderPlayerEvent.Pre event) {
         if (CMDCamClient.getCurrentPath() != null && CMDCamClient.getCurrentPath().cachedMode instanceof OutsideMode) {
             renderEntity = mc.getRenderManager().renderViewEntity;
-            
+
             mc.getRenderManager().renderViewEntity = mc.player;
         }
     }
-    
+
     @SubscribeEvent
     public void renderPlayerPost(RenderPlayerEvent.Post event) {
         if (CMDCamClient.getCurrentPath() != null && CMDCamClient.getCurrentPath().cachedMode instanceof OutsideMode) {
             mc.getRenderManager().renderViewEntity = renderEntity;
         }
     }
-    
+
     @SubscribeEvent
     public void cameraRoll(CameraSetup event) {
         event.setRoll(roll);
     }
-    
+
     public static long lastRenderTime;
-    
+
     public static boolean shouldPlayerTakeInput() {
         return true;
     }
-    
+
     public static void startSelectingTarget(String serverPath) {
         selectEntityMode = true;
         CamEventHandlerClient.serverPath = serverPath;
         mc.player.sendMessage(new TextComponentString("Please select a target either an entity or a block!"));
     }
-    
+
     private static boolean selectEntityMode = false;
     private static String serverPath = null;
-    
+
     @SubscribeEvent
     public void onPlayerInteract(PlayerInteractEvent event) {
         if (!selectEntityMode)
             return;
-        
+
         if (event instanceof EntityInteract) {
             if (serverPath != null)
                 PacketHandler.sendPacketToServer(new SelectTargetPacket(serverPath, new CamTarget.EntityTarget(((EntityInteract) event).getTarget())));
@@ -300,7 +301,7 @@ public class CamEventHandlerClient {
             selectEntityMode = false;
             serverPath = null;
         }
-        
+
         if (event instanceof RightClickBlock) {
             if (serverPath != null)
                 PacketHandler.sendPacketToServer(new SelectTargetPacket(serverPath, new CamTarget.BlockTarget(event.getPos())));
--- src/main/java/com/creativemd/cmdcam/client/mode/OutsideMode.java
+++ src/main/java/com/creativemd/cmdcam/client/mode/OutsideMode.java
@@ -11,25 +11,25 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class OutsideMode extends CamMode {
-    
+
     public EntityLivingBase camPlayer;
-    
+
     public OutsideMode(CamPath path) {
         super(path);
         if (path != null)
             this.camPlayer = new EntityZombie(Minecraft.getMinecraft().world);
     }
-    
+
     @Override
     public CamMode createMode(CamPath path) {
         return new OutsideMode(path);
     }
-    
+
     @Override
     public String getDescription() {
         return "the player isn't the camera, you can control him at every time";
     }
-    
+
     @Override
     @SideOnly(Side.CLIENT)
     public void onPathFinish() {
@@ -37,26 +37,24 @@ public class OutsideMode extends CamMode {
         Minecraft mc = Minecraft.getMinecraft();
         mc.setRenderViewEntity(mc.player);
     }
-    
+
     @Override
     public EntityLivingBase getCamera() {
         return camPlayer;
     }
-    
+
     @Override
     @SideOnly(Side.CLIENT)
     public void processPoint(CamPoint point) {
         super.processPoint(point);
-        
+
         Minecraft mc = Minecraft.getMinecraft();
         mc.setRenderViewEntity(camPlayer);
         if (camPlayer instanceof EntityPlayer)
             ((EntityPlayer) camPlayer).capabilities.isFlying = true;
         camPlayer.setPositionAndRotation(point.x, point.y, point.z, (float) point.rotationYaw, (float) point.rotationPitch);
-        double playerEyeHeight = mc.player.getEyeHeight();
-        double camEyeHeight = camPlayer.getEyeHeight();
         camPlayer.setLocationAndAngles(point.x, point.y - camPlayer.getEyeHeight() + mc.player.getEyeHeight(), point.z, (float) point.rotationYaw, (float) point.rotationPitch);
         camPlayer.setRotationYawHead(0);
     }
-    
+
 }
