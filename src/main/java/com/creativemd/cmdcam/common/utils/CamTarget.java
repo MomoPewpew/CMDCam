de356f33146ff224f00726e784798f2b14785dd2
MomoPewpew <50401575+MomoPewpew@users.noreply.github.com>
2023-02-05 15:55:48 +0100

    Loading SelfTarget cams from nbt no longer bugs

--- src/main/java/com/creativemd/cmdcam/common/utils/CamTarget.java
+++ src/main/java/com/creativemd/cmdcam/common/utils/CamTarget.java
@@ -10,21 +10,27 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraftforge.common.DimensionManager;
 
 public abstract class CamTarget {
-    
+
     private static HashMap<String, Class<? extends CamTarget>> targetTypes = new HashMap<>();
     private static HashMap<Class<? extends CamTarget>, String> targetTypesInverted = new HashMap<>();
-    
+
     public static void registerTargetType(Class<? extends CamTarget> targetClass, String id) {
         targetTypes.put(id, targetClass);
         targetTypesInverted.put(targetClass, id);
     }
-    
+
     public static Class<? extends CamTarget> getClassByID(String id) {
-        return targetTypes.get(id);
+    	World world = DimensionManager.getWorld(0);
+    	if (id.equals("self") && world != null) {
+    		return SelfTargetServer.class;
+    	} else {
+            return targetTypes.get(id);
+    	}
     }
-    
+
     public static CamTarget readFromNBT(NBTTagCompound nbt) {
         Class<? extends CamTarget> targetClass = getClassByID(nbt.getString("id"));
         if (targetClass != null) {
@@ -36,7 +42,7 @@ public abstract class CamTarget {
         }
         return null;
     }
-    
+
     static {
         registerTargetType(BlockTarget.class, "block");
         registerTargetType(VecTarget.class, "vec");
@@ -44,72 +50,72 @@ public abstract class CamTarget {
         registerTargetType(SelfTarget.class, "self");
         targetTypesInverted.put(SelfTargetServer.class, "self");
     }
-    
+
     public abstract Vec3d getTargetVec(World world, float partialTicks);
-    
+
     protected abstract void write(NBTTagCompound nbt);
-    
+
     protected abstract void read(NBTTagCompound nbt);
-    
+
     public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
         nbt.setString("id", targetTypesInverted.get(this.getClass()));
         write(nbt);
         return nbt;
     }
-    
+
     public static class VecTarget extends CamTarget {
-        
+
         public VecTarget() {
-            
+
         }
-        
+
         public VecTarget(Vec3d vec) {
             this.vec = vec;
         }
-        
+
         public Vec3d vec;
-        
+
         @Override
         public Vec3d getTargetVec(World world, float partialTicks) {
             return vec;
         }
-        
+
         @Override
         protected void write(NBTTagCompound nbt) {
             nbt.setDouble("x", vec.x);
             nbt.setDouble("y", vec.y);
             nbt.setDouble("z", vec.z);
         }
-        
+
         @Override
         protected void read(NBTTagCompound nbt) {
             this.vec = new Vec3d(nbt.getDouble("x"), nbt.getDouble("y"), nbt.getDouble("z"));
         }
-        
+
     }
-    
+
     public static class BlockTarget extends CamTarget {
-        
+
         public BlockTarget() {
-            
+
         }
-        
+
         public BlockTarget(BlockPos pos) {
             this.pos = pos;
         }
-        
+
         public BlockPos pos;
-        
+
         @Override
         public Vec3d getTargetVec(World world, float partialTicks) {
             return new Vec3d(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
         }
-        
+
         @Override
         protected void write(NBTTagCompound nbt) {
             nbt.setIntArray("data", new int[] { pos.getX(), pos.getY(), pos.getZ() });
         }
-        
+
         @Override
         protected void read(NBTTagCompound nbt) {
             int[] array = nbt.getIntArray("data");
@@ -117,31 +123,31 @@ public abstract class CamTarget {
                 throw new IllegalArgumentException("Invalid block target data=" + array);
             pos = new BlockPos(array[0], array[1], array[2]);
         }
-        
+
     }
-    
+
     public static class EntityTarget extends CamTarget {
-        
+
         public Entity cachedEntity;
         public String uuid;
-        
+
         public EntityTarget() {
-            
+
         }
-        
+
         public EntityTarget(String uuid) {
             this.cachedEntity = null;
             this.uuid = uuid;
         }
-        
+
         public EntityTarget(Entity entity) {
             this.cachedEntity = entity;
             this.uuid = entity.getCachedUniqueIdString();
         }
-        
+
         @Override
         public Vec3d getTargetVec(World world, float partialTicks) {
-            
+
             if (cachedEntity == null) {
                 for (Entity entity : world.getLoadedEntityList()) {
                     if (entity.getCachedUniqueIdString().equals(uuid)) {
@@ -150,56 +156,56 @@ public abstract class CamTarget {
                     }
                 }
             }
-            
+
             if (cachedEntity instanceof EntityLivingBase)
                 return ((EntityLivingBase) cachedEntity).getPositionEyes(partialTicks).subtract(new Vec3d(0, ((EntityLivingBase) cachedEntity).getEyeHeight(), 0));
             else if (cachedEntity != null)
                 return cachedEntity.getPositionEyes(partialTicks);
-            
+
             return null;
         }
-        
+
         @Override
         protected void write(NBTTagCompound nbt) {
             nbt.setString("uuid", uuid);
         }
-        
+
         @Override
         protected void read(NBTTagCompound nbt) {
             uuid = nbt.getString("uuid");
         }
-        
+
     }
-    
+
     public static class SelfTarget extends CamTarget {
-        
+
         public SelfTarget() {
-            
+
         }
-        
+
         @Override
         protected void write(NBTTagCompound nbt) {
-            
+
         }
-        
+
         @Override
         protected void read(NBTTagCompound nbt) {
-            
+
         }
-        
+
         @Override
         public Vec3d getTargetVec(World world, float partialTicks) {
-            
+
             Entity cachedEntity = Minecraft.getMinecraft().player;
-            
+
             if (cachedEntity instanceof EntityLivingBase)
                 return ((EntityLivingBase) cachedEntity).getPositionEyes(partialTicks).subtract(new Vec3d(0, ((EntityLivingBase) cachedEntity).getEyeHeight(), 0));
             else if (cachedEntity != null)
                 return cachedEntity.getPositionEyes(partialTicks);
-            
+
             return null;
         }
-        
+
     }
 
     public static class SelfTargetServer extends CamTarget {
